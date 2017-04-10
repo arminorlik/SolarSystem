31df8a723bd474452fc4a2e1686c8e6e11d761bb
Armin <armin.orlik@gmail.com>
2017-04-10 15:26:24 +0200

    RecyclerView z księżycami, ukrywanie jeśli planeta ich nie posiada, adapter z księżycami, nawigacja UP

--- app/src/main/java/com/example/armin/solarsystem/SolarObjectActivity.java
+++ app/src/main/java/com/example/armin/solarsystem/SolarObjectActivity.java
@@ -8,6 +8,8 @@ import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.view.View;
@@ -21,7 +23,7 @@ import java.io.IOException;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class SolarObjectActivity extends AppCompatActivity {
+public class SolarObjectActivity extends AppCompatActivity implements SolarObjectAdapter.SolarObjectClickedListener {
 
     public static final String OBJECT_KEY = "object";
     @BindView(R.id.objectImageView)
@@ -36,6 +38,10 @@ public class SolarObjectActivity extends AppCompatActivity {
     TextView objectTextView;
     @BindView(R.id.fab)
     FloatingActionButton fab;
+    @BindView(R.id.moonsLabel)
+    TextView moonsLabel;
+    @BindView(R.id.moonsRecyclerView)
+    RecyclerView moonsRecyclerView;
     private SolarObject solarObject;
 
     @Override
@@ -63,8 +69,6 @@ public class SolarObjectActivity extends AppCompatActivity {
         try { //ustawienie tekstu opisowego planety
             String text = SolarObject.loadFromAssest(this, solarObject.getText());
             objectTextView.setText(Html.fromHtml(text));
-
-
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -72,11 +76,28 @@ public class SolarObjectActivity extends AppCompatActivity {
         Glide.with(this)
                 .load(solarObject.getImagePath())
                 .into(objectImageView);
+
+        moonsRecyclerView.setVisibility(solarObject.hasMoon() ? View.VISIBLE : View.GONE); //sprawdzenie czy są księżyce, jeśli tak
+        //Visible jeśli nie ukrywanie recycleView
+        moonsLabel.setVisibility(solarObject.hasMoon() ? View.VISIBLE : View.GONE); //podobne ukrywanie w przypadku labelki
+
+        if (solarObject.hasMoon()) {
+            SolarObjectAdapter solarObjectAdapter = new SolarObjectAdapter(solarObject.getMoons());
+            solarObjectAdapter.setSolarObjectClickedListener(this);
+            moonsRecyclerView.setAdapter(solarObjectAdapter);
+            moonsRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+            moonsRecyclerView.setNestedScrollingEnabled(false); //wymuszenie aby jeden scroll był odpowiedzialny za przwijanie
+        }
     }
 
     public static void start(Activity activity, SolarObject solarObject) {
         Intent intent = new Intent(activity, SolarObjectActivity.class);
         intent.putExtra(OBJECT_KEY, solarObject);
         activity.startActivity(intent);
+    }
+
+    @Override
+    public void SolarObjectClicked(SolarObject solarObject) {
+        SolarObjectActivity.start(this, solarObject);
     }
 }
